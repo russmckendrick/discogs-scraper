e83b1ff0521086ef02b44ff4621a027b0264551c
Russ McKendrick <russ@mckendrick.io>
2023-04-16 15:24:53 +0100

    Update - add spotify embed

--- discogs_scraper.py
+++ discogs_scraper.py
@@ -2,9 +2,11 @@ import csv
 import os
 import re
 import json
-from datetime import datetime
-import discogs_client
+import requests
+import base64
 import time
+import discogs_client
+from datetime import datetime
 from urllib.request import urlretrieve
 from tqdm import tqdm
 
@@ -19,7 +21,9 @@ def sanitize_slug(slug):
 
 # Function to escape quotes
 def escape_quotes(text):
-    return text.replace('"', '\\"')
+    text = text.replace('"', '\\"')
+    text = re.sub(r'\s\(\d+\)', '', text)  # Remove brackets and numbers inside them
+    return text
 
 # Function to download an image
 def download_image(url, filename):
@@ -75,17 +79,77 @@ def format_notes(album_notes):
     )
     return formatted_notes
 
-# Function to load the Discogs API access token from a file
-def load_discogs_token(file_path):
-    with open(file_path, "r") as token_file:
-        token = token_file.read().strip()
-    return token
+# Spotify functions
+def get_spotify_id(artist, album_name):
+    token = get_spotify_token()
+    url = 'https://api.spotify.com/v1/search'
+    params = {
+        'q': f'artist:{artist} album:{album_name}',
+        'type': 'album',
+        'limit': 1
+    }
+    headers = {
+        'Authorization': f'Bearer {token}'
+    }
 
-# Load the Discogs API access token from a file
-discogs_token_file = "discogs_token.txt"
-discogs_access_token = load_discogs_token(discogs_token_file)
-if not discogs_access_token:
-    raise ValueError("Discogs API access token not found in the file '{}'".format(discogs_token_file))
+    response = requests.get(url, headers=headers, params=params)
+    if response.status_code == 200:
+        json_response = response.json()
+        if json_response.get('albums', {}).get('items'):
+            spotify_id = json_response['albums']['items'][0]['id']
+            return spotify_id
+    return None
+
+def get_spotify_token():
+    url = 'https://accounts.spotify.com/api/token'
+    headers = {
+        'Authorization': f'Basic {base64.b64encode(f"{spotify_client_id}:{spotify_client_secret}".encode()).decode()}'
+    }
+    data = {
+        'grant_type': 'client_credentials'
+    }
+
+    response = requests.post(url, headers=headers, data=data)
+    if response.status_code == 200:
+        json_response = response.json()
+        access_token = json_response.get('access_token')
+        return access_token
+    return None
+
+def get_spotify_embed_code(spotify_id):
+    if spotify_id:
+        return f'{{{{< spotify type="album" id="{spotify_id}" width="100%" height="500" >}}}}'
+    return ""
+
+class MissingSecretError(Exception):
+    pass
+
+def load_secrets(file_path, required_secrets):
+    with open(file_path, "r") as secret_file:
+        secrets = json.load(secret_file)
+
+    # Check for missing secrets
+    missing_secrets = [key for key in required_secrets if key not in secrets]
+    if missing_secrets:
+        raise MissingSecretError(f"Missing secrets in {file_path}: {', '.join(missing_secrets)}")
+
+    return secrets
+
+# Define required secrets
+required_secrets = ["discogs_access_token", "spotify_client_id", "spotify_client_secret"]
+
+# Load secrets from the file
+secrets_file = "secrets.json"
+try:
+    secrets = load_secrets(secrets_file, required_secrets)
+except MissingSecretError as e:
+    print(e)
+    exit(1)
+
+# Access the secrets
+discogs_access_token = secrets['discogs_access_token']
+spotify_client_id = secrets['spotify_client_id']
+spotify_client_secret = secrets['spotify_client_secret']
 
 # Create a Discogs client
 discogs_client = discogs_client.Client("AlbumScraper/0.1", user_token=discogs_access_token)
@@ -102,21 +166,30 @@ with open(discogs_csv_file, 'r') as csv_file:
     csv_file.seek(0)
     csv_reader = csv.DictReader(csv_file)
     next(csv_reader)  # Skip the header row
-    for row in tqdm(csv_reader, desc="Processing CSV Rows", total=total_rows - 1):  # Subtract 1 to account for the header row
-        catalog_no = escape_quotes(row['Catalog#'])
-        artist = escape_quotes(row['Artist'])
-        title = escape_quotes(row['Title'])
-        label = escape_quotes(row['Label'])
-        release_format = escape_quotes(row['Format'])
-        rating = row['Rating']
-        released = row['Released']
-        release_id = row['release_id']
-        date_added = row['Date Added']
-        media_condition = escape_quotes(row['Collection Media Condition'])
-        sleeve_condition = escape_quotes(row['Collection Sleeve Condition'])
-        notes = escape_quotes(row['Collection Notes'])
-        date_added = datetime.strptime(date_added, "%Y-%m-%d %H:%M:%S").strftime("%Y-%m-%d")
-        slug = sanitize_slug(title)
+
+    progress_bar = tqdm(csv_reader, total=total_rows - 1)
+
+    for row in progress_bar:
+        try:
+            catalog_no = escape_quotes(row['Catalog#'])
+            artist = escape_quotes(row['Artist'])
+            title = escape_quotes(row['Title'])
+            label = escape_quotes(row['Label'])
+            release_format = escape_quotes(row['Format'])
+            rating = row['Rating']
+            released = row['Released']
+            release_id = row['release_id']
+            date_added = row['Date Added']
+            media_condition = escape_quotes(row['Collection Media Condition'])
+            sleeve_condition = escape_quotes(row['Collection Sleeve Condition'])
+            notes = escape_quotes(row['Collection Notes'])
+            date_added = datetime.strptime(date_added, "%Y-%m-%d %H:%M:%S").strftime("%Y-%m-%d")
+            slug = sanitize_slug(title)
+
+            progress_bar.set_description(f"Processing {title} by {artist} ({release_id})")
+
+        except Exception as e:
+            print(f"Error processing row: {row}\nError: {e}")
 
         # Create post folder
         post_folder = os.path.join(output_folder, slug+"-"+release_id)
@@ -164,8 +237,17 @@ with open(discogs_csv_file, 'r') as csv_file:
         # Get discogs release URL
         release_url = release.url
 
+        spotify_id = get_spotify_id(artist, title)
+        spotify_embed_code = get_spotify_embed_code(spotify_id)
+
         # Add a 2-second delay between requests to avoid hitting the rate limit
         time.sleep(2)
+
+        # Check if spotify_embed_code is not empty
+        if spotify_embed_code.strip():
+            spotify_section = f"## Spotify\n{spotify_embed_code}\n"
+        else:
+            spotify_section = ""
 
         # Check if formatted_videos is not empty
         if formatted_videos.strip():
@@ -191,6 +273,8 @@ styles: {formatted_styles}
 
 ## Tracklisting
 {formatted_tracklist}
+
+{spotify_section}
 
 {videos_section}
 
